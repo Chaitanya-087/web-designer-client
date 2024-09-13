43ce7944e89f188318804535c98e3932e257a90a
ksidharth <keesari64@gmail.com>
2024-09-13 18:52:47 +0530

    added code mirror for html, css, js and code preview

---
+++ src/components/Editor.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import "codemirror/lib/codemirror.css";
+import "codemirror/theme/material.css";
+import "codemirror/mode/xml/xml";
+import "codemirror/mode/javascript/javascript";
+import "codemirror/mode/css/css";
+import 'codemirror/addon/edit/closebrackets';
+import 'codemirror/addon/edit/matchbrackets';
+import 'codemirror/addon/edit/closetag';
+import './codeeditor/CodeEditor.css'
+import { Controlled as ControlledEditor } from 'react-codemirror2';
+
+function Editor({ language, displayName, value, onChange }) {
+    function handleChange(editor, data, value) {
+        onChange(value);
+    }
+
+    return (
+        <div className="code-mirror-wrapper">
+          <div className="editor-title">
+          {displayName}
+          </div>
+            <ControlledEditor
+                onBeforeChange={handleChange}
+                value={value}
+                className="CodeMirror"
+                options={{
+                    lineWrapping: true,
+                    lint: true,
+                    mode: language,
+                    lineNumbers: true,
+                    theme: 'material',
+                    autoCloseBrackets: true,
+                    matchBrackets: true,
+                    highlightNonMatching: true,
+                    autoCloseTags: true,
+                    fullScreen: true,
+                }}
+            />
+        </div>
+    );
+}
+
+export default Editor;
--- src/components/codeeditor/CodeEditor.jsx
+++ src/components/codeeditor/CodeEditor.jsx
@@ -1,16 +1,76 @@
-import './codeEditor.css'
+import React, { useEffect, useState } from 'react';
+import './CodeEditor.css'
+import Editor from '../Editor';
 
-function CodeEditor()
-{
-    return(
+function CodeEditor() {
+    const [activePage, setActivePage] = useState('html');
+    const [html, setHtml] = useState('');
+    const [css, setCss] = useState('');
+    const [js, setJs] = useState('');
+    const [srcDoc, setSrcDoc] = useState('');
+
+    useEffect(() => {
+        const timeout = setTimeout(() => {
+            setSrcDoc(`
+                <html>
+                  <body>${html}</body>
+                  <style>${css}</style>
+                  <script>${js}</script>
+                </html>
+            `);
+        }, 250);
+
+        return () => clearTimeout(timeout);
+    }, [html, css, js]);
+
+    return (
         <>
             <div className="codeeditor-navbar">
-                <button>html</button>
-                <button>css</button>
-                <button>javascript</button>
-                <button>preview</button>
+                <button onClick={() => setActivePage('html')}>HTML</button>
+                <button onClick={() => setActivePage('css')}>CSS</button>
+                <button onClick={() => setActivePage('javascript')}>JavaScript</button>
+                <button onClick={() => setActivePage('preview')}>Preview</button>
+            </div>
+            <div className="codeeditor-content">
+                {activePage === 'html' && (
+                    <Editor
+                        language="xml"
+                        displayName="HTML"
+                        value={html}
+                        onChange={setHtml}
+                    />
+                )}
+                {activePage === 'css' && (
+                    <Editor
+                        language="css"
+                        displayName="CSS"
+                        value={css}
+                        onChange={setCss}
+                    />
+                )}
+                {activePage === 'javascript' && (
+                    <Editor
+                        language="javascript"
+                        displayName="JavaScript"
+                        value={js}
+                        onChange={setJs}
+                    />
+                )}
+                {activePage === 'preview' && (
+                    <div>
+                        <iframe
+                            srcDoc={srcDoc}
+                            title="output"
+                            sandbox="allow-scripts"
+                            width="100%"
+                            height="100%"
+                            className="full-screenable-node"
+                        />
+                    </div>
+                )}
             </div>
         </>
-    )
+    );
 }
+
 export default CodeEditor;
